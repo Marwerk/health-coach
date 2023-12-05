117f7caf11ea2189b25f895764a9ae43b9fad5bf
Marvin S. Werkmeister <marvin.saccarello@outlook.com>
2023-12-05 15:09:05 +0000

    Change routing

--- frontend-vue/src/router/index.js
+++ frontend-vue/src/router/index.js
@@ -7,26 +7,11 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/gyms',
-      name: 'gyms',
-      component: () => import('../views/GymView.vue')
-    },
-    {
-      path: '/testPost',
-      name: 'TestPost',
-      component: () => import('../views/TestPostView.vue')
-    },
+      component: () => import('../views/HomeView.vue')
+    }
   ]
 })
 
